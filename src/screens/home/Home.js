052e86548c8a2bb356af7037e2995df85aee8e8c
Aman Kumar <126275690+KrAman66@users.noreply.github.com>
2024-04-03 23:58:33 -0700

    update in Home.js

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
-import {
-  ScrollView,
-  StatusBar,
-  View,
-  ActivityIndicator,
-  Text,
-} from "react-native";
+import { ScrollView, StatusBar, View, ActivityIndicator, Text } from "react-native";
 import Header from "../../components/header/Header";
 import AnnouncementDetails from "../../components/announcment/AnnouncementDetails";
 import QuickLink from "../../components/quicklink/QuickLink";
 import Chat from "../../components/chat/Chat";
 import ChatType from "../../components/chat/ChatType";
 import BottomBar from "../../components/bottombar/BottomBar";
-import axios from "axios";
+import axios from 'axios';
 
 export default function Home() {
   const [latestNotice, setLatestNotice] = useState(null);
@@ -28,47 +22,39 @@ export default function Home() {
   const fetchData = async () => {
     try {
       const response = await axios.get(apiUrl);
-      // console.log("API response:", response.data);
+      // console.log('API response:', response.data);
       if (response.data.success) {
         const noticedata = response.data.data;
         // console.log('Original notice data:', noticedata);
         if (noticedata && noticedata.length > 0) {
-          noticedata.sort(
-            (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
-          );
+          noticedata.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
           // console.log('Sorted notice data:', noticedata);
           const latest = noticedata[0];
           // console.log('Latest notice:', latest);
           setLatestNotice(latest);
         } else {
-          console.log("No notices found.");
+          console.log('No notices found.');
         }
       } else {
-        setError(response.data.message || "Error fetching data.");
+        setError(response.data.message || 'Error fetching data.');
       }
       setIsLoading(false);
     } catch (error) {
-      console.error("Error fetching data:", error);
-      setError("Error fetching data. Please try again later.");
+      console.error('Error fetching data:', error);
+      setError('Error fetching data. Please try again later.');
       setIsLoading(false);
     }
   };
 
   // Function to format the date string
   const formatDate = (dateString) => {
-    const options = {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-    };
-    return new Date(dateString).toLocaleDateString("en-US", options);
+    const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return new Date(dateString).toLocaleDateString('en-US', options);
   };
 
   return (
     <>
-      <StatusBar backgroundColor={"#F08E0F"} />
+      <StatusBar backgroundColor={'#F08E0F'} />
       <View style={{ flex: 1 }}>
         <Header />
         <View style={{ flex: 1, paddingBottom: 10 }}>
@@ -80,10 +66,10 @@ export default function Home() {
             ) : latestNotice ? (
               <AnnouncementDetails
                 title={latestNotice.title}
-                date={formatDate(latestNotice.createdAt)}
+                date={formatDate(latestNotice.createdAt)} 
                 content={latestNotice.shortDec}
                 image={latestNotice.noticeImage}
-                page="home"
+                page='home'
               />
             ) : (
               <Text>No notices found.</Text>
@@ -98,3 +84,4 @@ export default function Home() {
     </>
   );
 }
+
